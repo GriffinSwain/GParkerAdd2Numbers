68d7532de01d39935fddfb271c0360eee71b61c5
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2022-10-20 15:10:44 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 //October 18, 2022
 //This program takes two user inputs and adds them together. It makes sure that the numbers are actually numbers then outputs the sum to the user. 
 //Peer Reviewed by: Harrison Busby
+//Griffin did a great job on his code. I really liked how if you entered a letter instead of numbers, it wouldnt kick you completely out. 
+//His code is organized and well thought out. Great job Griffin!
 
 string first;
 string second;
